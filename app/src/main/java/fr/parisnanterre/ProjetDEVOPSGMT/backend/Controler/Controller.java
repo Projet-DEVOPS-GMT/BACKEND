407b8d0846d51ca87709b2ac75cece92504e7338
ManelGuemm <manelguemmat6@gmail.com>
2024-11-22 17:25:59 +0100

    Add API Documentation

--- app/src/main/java/fr/parisnanterre/ProjetDEVOPSGMT/backend/Controler/Controller.java
+++ app/src/main/java/fr/parisnanterre/ProjetDEVOPSGMT/backend/Controler/Controller.java
@@ -1,12 +1,20 @@
 package fr.parisnanterre.ProjetDEVOPSGMT.backend.Controler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 
 @RestController
+@Tag(name = "Hello Controller")
 public class Controller {
     @GetMapping("/Hello")
+    @Operation(
+            summary = "Returns a welcome message",
+            description = "This endpoint returns a simple message saying 'Hello from API'"
+    )
     public String getHello(){
 return"Hello from API";
     }
 }
+;
